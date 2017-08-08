12c83f495030d925e520b2a57f77db477e3d50c5
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2017-08-08 09:29:03 -0700

    set up as proper function

--- index.php
+++ index.php
@@ -9,87 +9,108 @@
 
 
 <?php 
-//echo 'Current PHP version: ' . phpversion();
-# composer require
-require_once 'google-api-php-client-2.1.1/vendor/autoload.php';
-require 'vendor/autoload.php';
-
-$currenttime = date("h").date("i");
-$lastrequest = file_get_contents('build/docs/time.md');
-
-// html to markdown:
-use League\HTMLToMarkdown\HtmlConverter;
-
-$fileId = '1EaBo7TwYgl-Jzg9o_UcmP2mFMW5pVpirfRqp3STIxY4'; //whatever file you want to display— put it's id here
-
-if($currenttime >= ($lastrequest + 10) || $currenttime < $lastrequest ){
-  
-  # get developer key file:
-
-  $developerKey = file_get_contents('build/docs/developerkey.txt');
-  $developerKey = trim($developerKey); // removes any empty spaces
 
 
-  # google doc stuff:
-
-  $client = new Google_Client();
-  $client->setApplicationName("AIGA CDN Test");
-  $client->setDeveloperKey($developerKey);
-
-  $service = new Google_Service_Drive($client);
-
-  # google rest: Download files
-  # file id see global var before conditional
-  $response = $service->files->export($fileId, 'text/html', array(
-    'alt' => 'media' )); // text type
-  $content = $response->getBody()->getContents();
+$googleDoc = function($docID, $devKey){
 
 
-  # remove elements by tag name to clean up html:
-  $doc = new DOMDocument();
-  # load the HTML into the DomDocument object (this would be your source HTML)
-  $doc->loadHTML($content);
+  //echo 'Current PHP version: ' . phpversion();
+  # composer require
+  require_once 'google-api-php-client-2.1.1/vendor/autoload.php';
+  require 'vendor/autoload.php';
 
-  function removeElementsByTagName($tagName, $document) {
-    $nodeList = $document->getElementsByTagName($tagName);
-    for ($nodeIdx = $nodeList->length; --$nodeIdx >= 0; ) {
-      $node = $nodeList->item($nodeIdx);
-      $node->parentNode->removeChild($node);
+  $currenttime = date("h").date("i");
+  $lastrequest = file_get_contents('build/docs/time.md');
+
+  // html to markdown:
+  use League\HTMLToMarkdown\HtmlConverter;
+
+  //$fileId = '1EaBo7TwYgl-Jzg9o_UcmP2mFMW5pVpirfRqp3STIxY4'; //whatever file you want to display— put it's id here
+
+  $fileId = $docID;
+
+  if($currenttime >= ($lastrequest + 10) || $currenttime < $lastrequest ){
+    
+    # get developer key file:
+
+    //$developerKey = file_get_contents('build/docs/developerkey.txt');
+    //$developerKey = trim($developerKey); // removes any empty spaces
+
+    $devKey = trim($devKey);
+    $developerKey = $devKey;
+
+    # google doc stuff:
+
+    $client = new Google_Client();
+    $client->setApplicationName("AIGA CDN Test");
+    $client->setDeveloperKey($developerKey);
+
+    $service = new Google_Service_Drive($client);
+
+    # google rest: Download files
+    # file id see global var before conditional
+    $response = $service->files->export($fileId, 'text/html', array(
+      'alt' => 'media' )); // text type
+    $content = $response->getBody()->getContents();
+
+
+    # remove elements by tag name to clean up html:
+    $doc = new DOMDocument();
+    # load the HTML into the DomDocument object (this would be your source HTML)
+    $doc->loadHTML($content);
+
+    function removeElementsByTagName($tagName, $document) {
+      $nodeList = $document->getElementsByTagName($tagName);
+      for ($nodeIdx = $nodeList->length; --$nodeIdx >= 0; ) {
+        $node = $nodeList->item($nodeIdx);
+        $node->parentNode->removeChild($node);
+      }
     }
-  }
-  # remove extraneous style and script tags in google doc produced html:
-  removeElementsByTagName('script', $doc);
-  removeElementsByTagName('style', $doc);
-  removeElementsByTagName('link', $doc);
+    # remove extraneous style and script tags in google doc produced html:
+    removeElementsByTagName('script', $doc);
+    removeElementsByTagName('style', $doc);
+    removeElementsByTagName('link', $doc);
 
-  # output cleaned html
-  $content = $doc->saveHtml();
+    # output cleaned html
+    $content = $doc->saveHtml();
 
-  # convert html to markdown to clean html further
-  $converter = new HtmlConverter(array('strip_tags' => true));
-  $markdown = $converter->convert($content);
+    # convert html to markdown to clean html further
+    $converter = new HtmlConverter(array('strip_tags' => true));
+    $markdown = $converter->convert($content);
 
-  // parsedown for new/clean html:
-  $Parsedown = new Parsedown();
+    // parsedown for new/clean html:
+    $Parsedown = new Parsedown();
 
-  echo $Parsedown->text($markdown);
+    echo $Parsedown->text($markdown);
 
-  # put updated content into md files
-  file_put_contents('build/docs/cacheddoc.md', $markdown);
-  file_put_contents('build/docs/time.md', $currenttime);
+    # put updated content into md files
+    file_put_contents('build/docs/cacheddoc.md', $markdown);
+    file_put_contents('build/docs/time.md', $currenttime);
 
 
-} else{
-  
-  $cacheddoc = file_get_contents('build/docs/cacheddoc.md');
+  } else{
+    
+    $cacheddoc = file_get_contents('build/docs/cacheddoc.md');
 
-  // parsedown for new html:
-  $Cachedparsedown = new Parsedown();
-  echo $Cachedparsedown->text($cacheddoc);
+    // parsedown for new html:
+    $Cachedparsedown = new Parsedown();
+    echo $Cachedparsedown->text($cacheddoc);
 
- } ?>
+   } 
+}
 
-<a class="edit" href="https://docs.google.com/document/d/<?php echo $fileId; ?>/edit">
+
+
+$fileIdString = '1EaBo7TwYgl-Jzg9o_UcmP2mFMW5pVpirfRqp3STIxY4';
+$developerKey = file_get_contents('build/docs/developerkey.txt');
+
+$googleDoc($fileIdString, $developerKey);
+
+
+
+   ?>
+
+<a class="edit" href="https://docs.google.com/document/d/<?php echo $fileIdString; ?>/edit">
 	<div>edit this doc</div>
 </a>
 
